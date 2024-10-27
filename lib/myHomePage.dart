ebdc9ff35cfbb2f354c226a0daefd9f89fb423eb
Hamza-Bayar <21060638@stu.omu.edu.tr>
2024-10-27 23:34:25 +0300

    ScackBar ve anahtar ekrana eklendi
    Anahtar oluşturulduğunda ekranda gösterilmektedir.
    Şifreleme ve deşifreleme, başarılı veya başarısız olduklarında bir snackbar ile kullanıcı bilgilendirilmektedir.

--- lib/myHomePage.dart
+++ lib/myHomePage.dart
@@ -16,7 +16,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  late enc.Key key;
+  enc.Key? key;
   enc.IV iv = enc.IV.fromSecureRandom(16);
   final List<bool> _selectedNumber = [false, false, false];
   final List<Text> numbers = const [Text("128"), Text("192"), Text("256")];
@@ -32,58 +32,65 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisSize: MainAxisSize.max,
           children: [
-            button(Icons.image, "Show Decrypted Image", () async {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                    builder: (context) =>
-                        const ImagePage(fileName: "decryptedFoto.jpg")),
-              );
-            }),
-            button(Icons.file_copy, "Show Encrypted file", () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                    builder: (context) => const EncryptedFilePage(
-                        fileName: "encryptedBytes.enc")),
-              );
-            }),
-            button(Icons.delete, "Delete Files", () async {
-              await deleteFile("decryptedFoto.jpg");
-              await deleteFile("encryptedBytes.enc");
-            }),
-            button(Icons.camera_alt, "Open Camera", () {}),
-            ToggleButtons(
-              onPressed: (int index) {
-                setState(() {
-                  // The button that is tapped is set to true, and the others to false.
-                  for (int i = 0; i < _selectedNumber.length; i++) {
-                    _selectedNumber[i] = i == index;
-                  }
-                  if(index == 0) {
-                    selectedKeySizeByte = 16;
-                  } else if(index == 1) {
-                    selectedKeySizeByte = 24;
-                  } else if(index == 2) {
-                    selectedKeySizeByte = 32;
-                  }
-                });
-              },
-              borderRadius: const BorderRadius.all(Radius.circular(8)),
-              selectedBorderColor: Colors.deepPurple,
-              selectedColor: Colors.white,
-              fillColor: Colors.deepPurple,
-              color: Colors.deepPurple,
-              constraints: const BoxConstraints(
-                minHeight: 40.0,
-                minWidth: 80.0,
-              ),
-              isSelected: _selectedNumber,
-              children: numbers,
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                button(Icons.image, "Show Decrypted Image", () async {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) =>
+                            const ImagePage(fileName: "decryptedFoto.jpg")),
+                  );
+                }),
+                button(Icons.file_copy, "Show Encrypted file", () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const EncryptedFilePage(
+                            fileName: "encryptedBytes.enc")),
+                  );
+                }),
+                button(Icons.delete, "Delete Files", () async {
+                  await deleteFile("decryptedFoto.jpg");
+                  await deleteFile("encryptedBytes.enc");
+                }),
+                button(Icons.camera_alt, "Open Camera", () {}),
+                ToggleButtons(
+                  onPressed: (int index) {
+                    setState(() {
+                      // The button that is tapped is set to true, and the others to false.
+                      for (int i = 0; i < _selectedNumber.length; i++) {
+                        _selectedNumber[i] = i == index;
+                      }
+                      if (index == 0) {
+                        selectedKeySizeByte = 16;
+                      } else if (index == 1) {
+                        selectedKeySizeByte = 24;
+                      } else if (index == 2) {
+                        selectedKeySizeByte = 32;
+                      }
+                    });
+                  },
+                  borderRadius: const BorderRadius.all(Radius.circular(8)),
+                  selectedBorderColor: Colors.deepPurple,
+                  selectedColor: Colors.white,
+                  fillColor: Colors.deepPurple,
+                  color: Colors.deepPurple,
+                  constraints: const BoxConstraints(
+                    minHeight: 40.0,
+                    minWidth: 80.0,
+                  ),
+                  isSelected: _selectedNumber,
+                  children: numbers,
+                ),
+              ],
             ),
+            Text("Key: ${key?.bytes}")
           ],
         ),
       ),
@@ -101,27 +108,35 @@ class _MyHomePageState extends State<MyHomePage> {
         onTap: (value) async {
           if (value == 0) {
             setAESKey(selectedKeySizeByte, "hamza bayar");
-            print(key.bytes);
+            print(key!.bytes);
             Uint8List fileBytes = await loadFileAsBytes(
               "assets/images/Atomium.jpg",
             );
-            Uint8List encryptedBytes = await encryptFile(fileBytes);
-            await saveFile(
-              encryptedBytes,
-              "encryptedBytes.enc",
-            );
+            Uint8List? encryptedBytes = await encryptFile(context, fileBytes);
+            if(encryptedBytes != null) {
+              await saveFile(
+                encryptedBytes,
+                "encryptedBytes.enc",
+              );
+            } else {
+              deleteFile("encryptedBytes.enc");
+            }
           } else if (value == 1) {
             setAESKey(selectedKeySizeByte, "hamza bayar");
-            print(key.bytes);
+            print(key!.bytes);
             final directory = await getApplicationDocumentsDirectory();
             final filePath = p.join(directory.path, "encryptedBytes.enc");
             final file = File(filePath);
-            Uint8List decryptedFoto =
-                await decryptFile(file.readAsBytesSync(), key, iv);
-            await saveFile(
-              decryptedFoto,
-              "decryptedFoto.jpg",
-            );
+            Uint8List? decryptedFoto =
+                await decryptFile(context, file.readAsBytesSync(), key!, iv);
+            if(decryptedFoto != null) {
+              await saveFile(
+                decryptedFoto,
+                "decryptedFoto.jpg",
+              );
+            } else {
+              deleteFile("decryptedFoto.jpg");
+            }
           }
         },
       ),
@@ -152,7 +167,9 @@ class _MyHomePageState extends State<MyHomePage> {
 
     tempKeyByteList = paddingTheKey(tempKeyByteList, keySizeByte);
 
-    key = enc.Key(Uint8List.fromList(tempKeyByteList));
+    setState(() {
+      key = enc.Key(Uint8List.fromList(tempKeyByteList));
+    });
   }
 
   List<int> paddingTheKey(List<int> tempKeyByteList, int keySizeByte) {
@@ -171,20 +188,71 @@ class _MyHomePageState extends State<MyHomePage> {
     return data.buffer.asUint8List();
   }
 
-  Future<Uint8List> encryptFile(Uint8List fileBytes) async {
-    final encrypter = enc.Encrypter(enc.AES(key));
-    final encrypted = encrypter.encryptBytes(fileBytes, iv: iv);
+  Future<Uint8List?> encryptFile(BuildContext context, Uint8List fileBytes) async {
+    final encrypter = enc.Encrypter(enc.AES(key!));
 
-    return encrypted.bytes;
+    try {
+      final encrypted = encrypter.encryptBytes(fileBytes, iv: iv);
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          padding: const EdgeInsets.all(10),
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
+          backgroundColor: Colors.green,
+          content: const Text('Encryption completed', style: TextStyle(
+              fontSize: 17
+          ),),
+          duration: const Duration(seconds: 1),
+        ),
+      );
+      return encrypted.bytes;
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          padding: const EdgeInsets.all(10),
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
+          backgroundColor: Colors.redAccent,
+          content: const Text('An error occurred during the encryption process', style: TextStyle(
+              fontSize: 17
+          ),),
+          duration: const Duration(seconds: 5),
+        ),
+      );
+      return null;
+    }
   }
 
-  Future<Uint8List> decryptFile(
-      Uint8List encryptedBytes, enc.Key key, enc.IV iv) async {
+  Future<Uint8List?> decryptFile(BuildContext context, Uint8List encryptedBytes,
+      enc.Key key, enc.IV iv) async {
     final encrypter = enc.Encrypter(enc.AES(key));
-    final decrypted =
-        encrypter.decryptBytes(enc.Encrypted(encryptedBytes), iv: iv);
 
-    return Uint8List.fromList(decrypted);
+    try {
+      final decrypted = encrypter.decryptBytes(enc.Encrypted(encryptedBytes), iv: iv);
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          padding: const EdgeInsets.all(10),
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
+          backgroundColor: Colors.green,
+          content: const Text('Decryption completed', style: TextStyle(
+              fontSize: 17
+          ),),
+          duration: const Duration(seconds: 1),
+        ),
+      );
+      return Uint8List.fromList(decrypted);
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          padding: const EdgeInsets.all(10),
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
+          backgroundColor: Colors.redAccent,
+          content: const Text('An error occurred during the decryption process.', style: TextStyle(
+            fontSize: 17
+          ),),
+          duration: const Duration(seconds: 5),
+        ),
+      );
+      return null;
+    }
   }
 
   Future<void> saveFile(Uint8List fileAsBytes, String fileName) async {
